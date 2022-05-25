dfecbc6f49e7722b30138e80cd831714c81ece27
Sr-Mourao <felipemourao6124@gmail.com>
2022-05-25 20:56:30 -0300

    formulario com Vuejs e Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <h3>Empresa Ltda Mourão's</h3>
+      </div>
+
+      <v-spacer></v-spacer>
+      <NovoCadastro />
+
+      
+    </v-app-bar>
+
+    <v-main></v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld";
+import NovoCadastro from "./components/NovoCadastro.vue"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+    NovoCadastro,
+},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/NovoCadastro.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="500">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="primary lighten-2" dark v-bind="attrs" v-on="on">
+          Novo Cadastro
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title class="text-h5 info"> Cadastro </v-card-title>
+
+        <v-card-text>
+          <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="10"
+            label="Nome"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="lastname"
+            :rules="nameRules"
+            :counter="10"
+            label="Sobrenome"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+          <v-checkbox
+            v-model="checkbox"
+            :rules="[(v) => !!v || 'Você deve concorda e continuar']"
+            label="Você concorda!"
+            required
+          ></v-checkbox>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" @click="dialog = false"> Cadastra </v-btn>
+          <v-btn color="secondary" @click="dialog = false"> Cancelar </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+      email: "",
+      emailRules: [
+        (v) => !!v || "E-mail invalido",
+        (v) => /.+@.+\..+/.test(v) || "o e-mail deve ser valido",
+      ],
+      checkbox: false,
+    };
+  },
+};
+</script>
